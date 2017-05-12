99bc7250c6f470f22c368370c841c92fc9e05ea0
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2017-05-12 13:02:11 -0700

    set chefdk version for bootstrap.ps1 file

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -34,6 +34,8 @@ function Install-Project {
     [string] $json_attributes
   )
 
+  $version = '1.3.43.1'
+
   if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator")) {
       Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
