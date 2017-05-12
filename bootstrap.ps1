8f860f7b3fc247c4a18fc237f99a124a6fecf34d
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2017-05-12 13:03:34 -0700

    Updated bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -34,7 +34,7 @@ function Install-Project {
     [string] $json_attributes
   )
 
-  $version = '1.3.43.1'
+  $version = '1.3.43.1' 
 
   if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator")) {
