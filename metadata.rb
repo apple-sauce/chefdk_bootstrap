c4f527378e9b17e8b16ea50c4b0d1b549d96b7a4
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2017-05-19 13:13:37 -0700

    cleaned up code

--- metadata.rb
+++ metadata.rb
@@ -23,7 +23,7 @@ version           '0.1.4'
 chef_version      '>= 12.1' if respond_to?(:chef_version)
 
 depends           'chocolatey'
-depends           'vscode'
+# depends           'vs_code'
 # depends           'chefdk'
 # depends           'vagrant'
 # depends           'virtualbox', '~> 1.0.3'
