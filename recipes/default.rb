71c8c0a7b46e3bf61c9972e8f0509fde7faf266d
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2017-05-19 15:05:05 -0700

    Testing vbox install

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,27 +16,28 @@
 #   checksum node['chefdk_bootstrap']['chefdk']['checksum']
 # end
 #
-case node['platform_family']
-when 'windows'
-  chocolatey_package 'conemu' do
-    # If conemu was installed outside chocolatey, it could be running this
-    # installation script and we don't want to touch it
-    not_if '(& "C:\Program Files\ConEmu\ConEmu\ConEmuC.exe" /IsConEmu); $LASTEXITCODE -eq 1'
-    guard_interpreter :powershell_script
-  end
+# case node['platform_family']
+# when 'windows'
+#   chocolatey_package 'conemu' do
+#     # If conemu was installed outside chocolatey, it could be running this
+#     # installation script and we don't want to touch it
+#     not_if '(& "C:\Program Files\ConEmu\ConEmu\ConEmuC.exe" /IsConEmu); $LASTEXITCODE -eq 1'
+#     guard_interpreter :powershell_script
+#   end
 
-  chocolatey_package 'visualstudiocode' do
-  end
-end
-# windows_package 'virtualbox' do
-#   action :install
-#   installer_type :custom
-#   version node['chefdk_bootstrap']['virtualbox']['version']
-#   source node['chefdk_bootstrap']['virtualbox']['url']
-#   checksum node['chefdk_bootstrap']['virtualbox']['checksum']
-#   options '-s'
-#   returns [0]
+#   chocolatey_package 'visualstudiocode' do
+#   end
 # end
+
+windows_package 'virtualbox' do
+  action :install
+  installer_type :custom
+  version node['chefdk_bootstrap']['virtualbox']['version']
+  source node['chefdk_bootstrap']['virtualbox']['url']
+  checksum node['chefdk_bootstrap']['virtualbox']['checksum']
+  options '-s'
+  returns [0]
+end
 
 # windows_package 'Vagrant' do
 #   action :install
