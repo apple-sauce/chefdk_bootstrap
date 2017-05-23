4db63569b2536bcf89f63280ac07be3d947a9c80
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2017-05-23 12:05:14 -0700

    adding choco into the recipe vs the bootstrap powershell

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -11,29 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-  
-  $env:HOME = $env:USERPROFILE
-  $env:Path += ";C:\opscode\chefdk\bin"
-  
-  # run chef-client to bootstrap this machine 
-  . { Invoke-WebRequest -useb https://omnitruck.chef.io/install.ps1 } | Invoke-Expression; install -version 1.3.43 -channel stable  -project chefdk
-  
-  # need to set location for downloading of chefdk_bootstrap as well as create folder cookbook 
-  Set-Location "~\AppData\Local\Temp\"
-  mkdir cookbooks
-  Set-Location "~\AppData\Local\Temp\cookbooks"
 
-  # downloading of the chefdk_bootstrap cookbook to local machine
-  C:\opscode\chefdk\embedded\git\bin\git.exe clone https://github.com/apple-sauce/chefdk_bootstrap.git
+$env:HOME = $env:USERPROFILE
+$env:Path += ";C:\opscode\chefdk\bin"
 
-  # install chocolatey
-  # Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Make sure you're an admin of this machine 
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+  [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
 
-  # set location back to cookbook in order to run berks to get dependencies
-  Set-Location "~\AppData\Local\Temp\cookbooks\chefdk_bootstrap"
-  berks vendor
+# run chef-client to bootstrap this machine 
+. { Invoke-WebRequest -useb https://omnitruck.chef.io/install.ps1 } | Invoke-Expression; install -version 1.3.43 -channel stable  -project chefdk
 
-  # run chef client to converge machine
-  chef-client -A -z -l error -o 'chefdk_bootstrap'
+# need to set location for downloading of chefdk_bootstrap as well as create folder cookbook 
+Set-Location "~\AppData\Local\Temp\"
+mkdir cookbooks
+Set-Location "~\AppData\Local\Temp\cookbooks"
 
-  Write-Host "`n`nCongrats fellow Chefee! Your workstation is now set up for Chef Development!"
+# downloading of the chefdk_bootstrap cookbook to local machine
+C:\opscode\chefdk\embedded\git\bin\git.exe clone https://github.com/apple-sauce/chefdk_bootstrap.git
+
+# install chocolatey
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# set location back to cookbook in order to run berks to get dependencies
+Set-Location "~\AppData\Local\Temp\cookbooks\chefdk_bootstrap"
+berks vendor
+
+# run chef client to converge machine
+chef-client -A -z -l error -o 'chefdk_bootstrap'
+
+Write-Host "`n`nCongrats fellow Chefee! Your workstation is now set up for Chef Development!"
