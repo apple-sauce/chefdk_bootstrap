da239a7a0078810ca5b65c075fe3ce2dbb15defa
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2018-06-21 16:15:18 -0700

    third fix to the message box

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -26,22 +26,17 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
-[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
-$msgBoxInput =  [System.Windows.MessageBox]::Show('Installing Chefdk will reboot your laptop as soon as installation is complete. Please save all unsaved work before hitting OK, otherwise hit Cancel to abort.','Chefdk Install','YesNoCancel','Error')
-
-  switch  ($msgBoxInput) {
-  'Yes' {
-          Write-Host "Your laptop will reboot after the installation"
-        }
-  'No'  {
-          Write-Warning "You have choosen no. Chefdk will not be installed"
-          Break 
-        }
-  'Cancel' {
-          Write-Warning "Cancelling the install. Chefdk will not be installed"
-          Break 
-        }
-  }
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$OUTPUT= [System.Windows.Forms.MessageBox]::Show('Installing Chefdk will reboot your laptop as soon as installation is complete. Please save all unsaved work before hitting OK, otherwise hit Cancel to abort.','Chefdk Install', 1)
+if ($OUTPUT -eq "OK" ) 
+{
+	Write-Host "Your laptop will reboot after the installation"
+} 
+else 
+{ 
+	Write-Warning "Cancelling the install. Chefdk will not be installed"
+	Break 
+}
 
 try {
   # Install ChefDK from chef omnitruck, unless installed already
