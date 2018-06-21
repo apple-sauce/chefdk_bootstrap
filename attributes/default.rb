b3634d2767bb4753a968fd12f4fe7b1484ffdc8f
Jamie Abellanosa <Jamie.Abellanosa@gatesfoundation.org>
2018-06-21 14:29:57 -0700

    updated vitualbox, vagrant and chefdk

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,12 +20,12 @@ when 'mac_os_x'
   default['chefdk_bootstrap']['virtualbox']['version'] = 'TBD'
 when 'windows'
   # -----Virtual Box settings--------------
-  default['chefdk_bootstrap']['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/5.1.22/VirtualBox-5.1.22-115126-Win.exe'
-  default['chefdk_bootstrap']['virtualbox']['version'] = '5.1.22'
+  default['chefdk_bootstrap']['virtualbox']['url'] = 'http://download.virtualbox.org/virtualbox/5.2.12/VirtualBox-5.2.12-122591-Win.exe'
+  default['chefdk_bootstrap']['virtualbox']['version'] = '5.2.12'
   # -----Vagrant settings -----------------
-  default['chefdk_bootstrap']['vagrant']['source'] = 'https://releases.hashicorp.com/vagrant/1.9.5/vagrant_1.9.5.msi'
-  default['chefdk_bootstrap']['vagrant']['checksum'] = '8ce945cd01d1273b628aa101e035716c246dfbddad29cccc8d96c873f77d386b'
-  default['chefdk_bootstrap']['vagrant']['version'] = '1.9.5'
+  default['chefdk_bootstrap']['vagrant']['source'] = 'https://releases.hashicorp.com/vagrant/2.1.1/vagrant_2.1.1_x86_64.msi'
+  default['chefdk_bootstrap']['vagrant']['checksum'] = '0A1267CA0FBFC768A2F8AE6C5D70E9BBDB95EA960EB2BE5D524A327CCB32B20C'
+  default['chefdk_bootstrap']['vagrant']['version'] = '2.1.1'
 when 'debian', 'rhel'
   default['chefdk_bootstrap']['virtualbox']['version'] = 'TBD'
 end
--- metadata.rb
+++ metadata.rb
@@ -15,6 +15,7 @@
 name              'chefdk_bootstrap'
 maintainer        'Apple-Sauce'
 maintainer_email  'Apple-Sauce@Saucey.com'
+chef_version      '>= 12.1' if respond_to?(:chef_version)
 license           'All Rights Reserved'
 description       'Installs/Configures laptop with chefdk and other tools needed to code with chef'
 long_description  'Installs/Configures the following tools:
@@ -24,6 +25,4 @@ long_description  'Installs/Configures the following tools:
                    Visual Studio Code: latest
                    Vagrant: 1.9.5
                    Virtual Box: 5.1.22'
-version           '0.2.5'
-
-chef_version      '>= 12.1' if respond_to?(:chef_version)
+version           '0.2.6'
